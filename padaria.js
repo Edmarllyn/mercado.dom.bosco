db652ae9d61798944346708f718ab0b15780a734
Wellington da Silva <nenem12@icloud.com>
2024-10-10 21:36:04 -0300

    treino
    treino

---
+++ padaria.js
@@ -0,0 +1,11 @@
+// Aqui adicionamos um evento de escutar o click do mause sobre o ícone de Hamburguer.
+hamburguerButton.addevEntListener("click", function() {
+    mobileMenu.classList.add("flex")
+    // Criamos uma função que a ouvir o click do mouse, ele adicione uma class, que está fazendo nossa caixa (div) aparecer, ou seja, ficar visivel.
+
+});
+
+//Criamos uma função que a ouvir o click do Mouse, ele REMOVA uma Class, que está fazendo nossa caixa (Div) desaparecer, ou seja, ficar invisível.
+closeButton.addEventListener("click", function() {
+    mobileMenu.classList.remove("flex");
+});
